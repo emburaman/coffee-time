c698510a9846d064b78a9e17d0086d198f8ba293
Lincoln Souza <emburaman@gmail.com>
2021-01-31 16:57:03 -0500

    fix sendNotification

--- public/push.js
+++ public/push.js
@@ -8,5 +8,14 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {}
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/ex3IHN9qoNg:APA91bE4mcdC22adk-LTd8pHtPxRTDk1OBcxghod4cYiGapjwNA7UIzxQsdmhC6jcl940wKRtK8ccXWNb-oPbqlh4N-qd5jJVeKYfIiokrdIOxDIkr9VLvEuffxc5gcZdUZfXIl0pRvM",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BJaKtt5lCSLMAKojvQaVzwWGXx2BD1or78SyeL_eBtP5CCfFJYihUUamOExX0n8igicI4gAu6mgGo5mY2apLT68",
+    auth: "JF_kEnTcrfEsXOzi-d0d8Q",
+  },
+};
 push.sendNotification(sub, 'test message')
